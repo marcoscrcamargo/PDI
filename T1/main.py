e95f4972d66e0a91793f53c37ff3fe74f1736ffd
Marcos Camargo <marcoscrcamargo@gmail.com>
2018-03-16 10:01:50 -0300

    alguns bugs durante a quantização e com prints de debug

--- T1/main.py
+++ T1/main.py
@@ -2,66 +2,100 @@
 # coding: utf-8
 
 # Marcos Cesar Ribeiro de Camargo - 9278045
-# SCC251 - 1/2018
-# Trabalho 1 - Gerador de imagens
+# SCC251 							 1/2018
+# Trabalho 1 			 Gerador de imagens
 
 # imports
 import numpy as np
 import random
 import math
+import imageio
 
 # Funções para geração da imagem da cena.
 
 # Função 1 - x + y.
-def f_one(x, y, Q):
-	return (x + y)
+def f_one(m, Q):
+	for (x, y), value in np.ndenumerate(m):
+		m[x, y] = (x + y)
+	return m
 
 # Função 2 - |sin(x/Q) + sin(y/Q)|.
-def f_two(x, y, Q):
-	return abs( math.sin(x/Q) + math.sin(y/Q) )
+def f_two(m, Q):
+	for (x, y), value in np.ndenumerate(m):
+		m[x, y] = abs(math.sin(x/Q) + math.sin(y/Q))
+	return m
 
 # Função 3 - (x/Q) - sqrt(y/Q).
-def f_three(x, y, Q):
-	return (x/Q) - math.sqrt(y/Q)
+def f_three(m, Q):
+	for (x, y), value in np.ndenumerate(m):
+		m[x, y] = ((x/Q) - math.sqrt(y/Q))
+	return m
 
 # Função 4 - valor aleatório continuo entre 0 e 1.
-def f_four(x, y, Q):
-	return random.random()
+def f_four(m, Q):
+	for (x, y), value in np.ndenumerate(m):
+		m[x, y] = random.random()
+	return m
+
 
 # Função 5 - caminho aleatório.
-def f_five(x, y, Q):
-	return 0
+def f_five(m, Q):
+	C = m.shape[0]
+	moves = int(C*C/4)
+	# Passo 1
+	x = 0
+	y = 0
+	m[x, y] = 1.0
 
-# Função que aplica uma função em todos os elementos da matriz.
-def gen_scene(m, f, Q):
-	for (x,y), value in np.ndenumerate(m):
-		m[x,y] = f(x, y, Q)
+	for i in range(moves):
+		dx  = random.randint(-1, 1)
+		x = ((x + dx) % C)
+		m[x, y] = 1.0
+
+		dy = random.randint(-1, 1)
+		y = ((y + dy) % C)
+		m[x, y] = 1.0
 
 	return m
 
 # Função que calcula o máximo local dos d pontos a partir
 # 	de (i,j) horizontal e verticalmente
 def local_max(scene, i, j, d):
-	return scene[i*d:(i+1)*d:1, j*d:(j+1)*d:1].max()
+	return scene[int(i*d):int((i+1)*d):1, int(j*d):int((j+1)*d):1].max()
 
 # Função que calcula os valores da imagem digital
 #  a partir da cena utilizando o máximo local.
 def gen_image(image, scene, B, f_max=local_max):
 	d = scene.shape[0]/image.shape[0]
 
+	# Normalizando valores entre 0 e 255
+	# scene = (scene* ((np.iinfo(np.int8).max-np.iinfo(np.int8).min)/(scene.max() - scene.min())) )
+	scene = 255*((scene)/(scene.max()))
+
+	# Convertendo para int32
+	scene = scene.astype('int')
+
+	# Quantização
+	print("gerando img")
+	print(scene.min())
+	print(scene.max())
+	print(scene)
+	scene = scene >> (8-B)
+
+	# Máximo local
 	for (i,j), value in np.ndenumerate(image):
-		image[i,j] = np.uint8(f_max(scene, i, j, d)) 
-						& (255 >> 8-B) # Mudar esse 8 hardcoded ?
+		image[i,j] = (f_max(scene, i, j, d))
 
 	return image
 
+# Função que calcula o erro médio quadratico para duas matrizes.
 def RMSE(g, R):
 	return math.sqrt(sum(sum( (g-R)**2 )))
 
 def main():
 	# Recebendo parametros.
 	# Nome do arquivo.
-	fname = input()
+	fname = str(input().rstrip())
 	# Tamanho da lateral da cena (C x C).
 	C = int(input())
 	# Função a ser utilizada (1, 2, 3, 4, 5).
@@ -75,7 +109,7 @@ def main():
 	# Semente S para a função random.
 	S = int(input())
 
-	# Iniciando valor da semente para ramdom
+	# Iniciando valor da semente para random.
 	random.seed(S)
 
 	# Gerando a imagem da cena.
@@ -91,13 +125,26 @@ def main():
         5: f_five,
 	}
 	# Gerando a cena de acordo com a função escolhida.
-	scene = gen_scene(scene, functions.get(f), Q)
-
+	scene =  functions.get(f)(scene, Q)
+	imageio.imwrite("scene.png", scene)
+	print("cena:")
+	print(scene)
+	print(scene.max())
+	print(scene.min())
 	# Gerando imagem digital a partir da cena.
-	image = np.zeros((N, N), dtype=np.uint8)
+	image = np.zeros((N, N))
 	image = gen_image(image, scene, B)
+	imageio.imwrite("image.png", image)
 
 	# Comparação
+	R = np.load(fname) # Carregando arquivo para comparação.
+
+	error = RMSE(image, R)
+	print("RESPOSTA")
+	print(image)
+	print("R-TRUE")
+	print(R)
+	print(error)
 
 if __name__ == "__main__":
     main()
