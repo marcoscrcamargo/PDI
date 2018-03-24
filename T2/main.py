fe9fee5fc7196d5d9717b0e8989241d6d0eac506
Marcos Camargo <marcoscrcamargo@gmail.com>
2018-03-24 20:09:11 -0300

    Todas funções implementadas, resultados diferentes dos esperados.

--- T2/main.py
+++ T2/main.py
@@ -3,11 +3,12 @@
 
 # Marcos Cesar Ribeiro de Camargo - 9278045
 # SCC251 							 1/2018
-# Trabalho 2 			 	super-resolução
+# Trabalho 2 			 	Super-resolução
 
 import numpy as np
 import imageio
 import math
+import matplotlib.pyplot as plt
 
 # Função para a leitura das 4 imagens de baixa qualidade.
 def read_images(fname):
@@ -23,17 +24,32 @@ def read_images(fname):
 def ha(img):
 	# Calculando os ids, e a quantidade de vezes que cada valor se repete para o id.
 	(ids, qnt) = np.unique(img, return_counts=True)
-	# Inicia uma matriz com 255 valores em zero.
-	h = np.zeros(255)
+
+	# Inicia uma matriz com 256 valores em zero.
+	h = np.zeros(256)
 
 	# Calculo do histograma.
 	for i in range(ids.shape[0]):
-		h[int(ids[i])] = qnt[i] 
+		h[int(ids[i])] = qnt[i]
 
 	# Calculo do histograma acumulado.
 	ha = np.cumsum(h)
 
 	return ha
+
+# Função que faz a equalização por histograma dado uma imagem e histograma.
+def histogram_equalization(img, hist):
+	L = 256
+	N = img.shape[0]
+
+	for (i, j), v in np.ndenumerate(img):
+		img[i, j] = ((L - 1)/(N**2))*hist[img[i, j]]
+
+	return img
+
+# Função que faz o ajuste gamma sobre a imagem img.
+def gamma_adjust(img, gamma):
+	return np.uint8(255.0*((img/255.0)**gamma))
 
 # Função que gera uma imagem H com super-resolução a partir de um
 # 	array de imagens L.
@@ -56,7 +72,6 @@ def SR(L):
 def RMSE(H, Hr):
 	return math.sqrt( (sum(sum( (H-Hr)**2 )))/(Hr.shape[0]**2) )
 
-
 def main():
 	# Recebendo parametros (inputs).
 	## Nome do arquivo para imagem de baixa resolução.
@@ -66,13 +81,35 @@ def main():
 	## Método de realce.
 	opt = int(input())
 	## Parâmetro do realce gamma.
-	gamma = int(input())
+	gamma = float(input())
 
 	# Lendo as imagens de baixa qualidade.
 	L = read_images(imglow)
+	# Método de realce 1.
+	# Função de transferencia individual.
+	if(opt == 1):
+		for i in range(len(L)):
+			hist = ha(L[i])
+			L[i] = histogram_equalization(L[i], hist)
+
+	# Método de realce 2.
+	# Função de transferencia conjunta.
+	elif(opt == 2):
+		hist = 0
+		for Li in L:
+			hist += ha(Li)
+
+		for i in range(len(L)):
+			L[i] = histogram_equalization(L[i], hist)
+
+	# Método de realce 3.
+	# Função de ajuste gamma.
+	elif(opt == 3):
+		for i in range(len(L)):
+			L[i] = gamma_adjust(L[i], gamma)
 
 	# Gerando imagem com super-resolução.
-	H = SR(L)	
+	H = SR(L)
 
 	# Abrindo imagem de alta qualidade.
 	Hr = imageio.imread(imghigh + ".png")
